09d6fdc6f4763f17f5eadbd7ce89a21538e440ec
Aileon <767872313@qq.com>
2020-06-04 01:20:42 +0800

    describe the instruction of UserDelete;modify instructions of other modules;

--- UserManagementBackend/__init__.py
+++ UserManagementBackend/__init__.py
@@ -25,16 +25,31 @@ RuntimeError  # 连接数据库失败
 """
 本包使用方法：
 1、注册功能
-
+from UserManagementBackend.UserRegister import Register # 导入
+from UserManagementBackend.UserManagementError import *   # 导入
 Register().register(用户名, 用户密码, 用户身份)  # 将用户名，用户密码，用户身份写入数据库，无返回值
+
 可能产生的错误：
-InputIsNoneError  # 输入有误
+InputIsNoneError  # 输入为空
 IdentityDoesNotExistError  # 注册身份不存在
 UserExistError # 用户名已存在，不可进行注册
 
 2、登陆功能
+from UserManagementBackend.UserLogin import LoginFactoryEncrypt, LoginUser # 导入
+from UserManagementBackend.UserManagementError import *   # 导入
 login_user = LoginUser(LoginFactoryEncrypt().create_user(用户名, 用户密码))  # 读取数据库，返回一个包含该用户所有信息的LoginUser对象
+
 可能产生的错误：
 PasswordError  # 密码错误
 UserDoesNotExistError  # 用户不存在
+
+3、删除功能
+from UserManagementBackend.UserDelete import Delete # 导入
+from UserManagementBackend.UserManagementError import *   # 导入
+方式1：Delete().delete(用户名)  # 删除单个用户，输入参数为str
+方式2：Delete().delete(用户名的集合)  # 删除一组用户，输入参数为set，set的元素为str
+
+可能产生的错误：
+UnknownInputTypeError  # 输入参数的类型有误
+UserDoesNotExistError  # 要删除的用户不存在
 """
